9b8c12986c45365af1c1e7e5306d30771f92483f
JCtrash <spam_box_jc@zoho.eu>
2017-10-11 19:18:07 +0300

    Lab3 was added.

---
+++ Lab3/parallel_programming_3/Lab3.cs
@@ -0,0 +1,84 @@
+﻿/*
+    Parallel and Allotted Calculations.
+    Lab #2: Threads in C#.
+
+    The code was written by
+        Krisak Ivan,
+        student of IO-53, KPI.
+    October 5, 2017.
+
+    Functions:
+    c = a.sort() * (ma * me) + b.sort();
+    mg = mk.transpose() * (mh * mf);
+    o = (r + s).sort() * (mt * mp);
+*/
+using System;
+using System.Threading;
+
+namespace parallel_programming_3
+{
+    class Lab3
+    {
+        static void Main(String[] args)
+        {
+            Math m = new Math();
+            Thread t1 = new Thread(() =>
+            {
+                Console.WriteLine("Thread #1 has been initialized");
+
+                Math.Vector a = m.fill_vector(1.0);//m.read_vector();
+                Math.Vector b = m.fill_vector(1.0);//m.read_vector();
+                Math.Matrix ma = m.fill_matrix(1.0);//m.read_matrix();
+                Math.Matrix me = m.fill_matrix(1.0);//m.read_matrix();
+
+                Thread.Sleep(15);
+                Math.Vector c = m.function_1(a, b, ma, me);
+                Thread.Sleep(15);
+                m.print(c);
+
+                Console.WriteLine("Thread #1 has finished the execution");
+            });
+            t1.Priority = ThreadPriority.Normal;
+            t1.Start();
+            Thread t2 = new Thread(() =>
+            {
+                Console.WriteLine("Thread #2 has been initialized");
+
+                Math.Matrix mk = m.fill_matrix(1.0);//m.read_matrix();
+                Math.Matrix mh = m.fill_matrix(1.0);//m.read_matrix();
+                Math.Matrix mf = m.fill_matrix(1.0);//m.read_matrix();
+
+                Thread.Sleep(15);
+                Math.Matrix mg = m.function_2(mk, mh, mf);
+                Thread.Sleep(15);
+                m.print(mg);
+
+                Console.WriteLine("Thread #2 has finished the execution");
+            });
+            t2.Priority = ThreadPriority.BelowNormal;
+            t2.Start();
+            Thread t3 = new Thread(() =>
+            {
+                Console.WriteLine("Thread #3 has been initialized");
+
+                Math.Vector r = m.fill_vector(1.0);//m.read_vector();
+                Math.Vector s = m.fill_vector(1.0);//m.read_vector();
+                Math.Matrix mt = m.fill_matrix(1.0);//m.read_matrix();
+                Math.Matrix mp = m.fill_matrix(1.0);//m.read_matrix();
+
+                Thread.Sleep(15);
+                Math.Vector o = m.function_3(r, s, mt, mp);
+                Thread.Sleep(15);
+                m.print(o);
+
+                Console.WriteLine("Thread #3 has finished the execution");
+            });
+            t3.Priority = ThreadPriority.AboveNormal;
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+        }
+    }
+}
---
+++ Lab3/parallel_programming_3/Math.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace parallel_programming_3
+{
+    class Math
+    {
+        static private Int32 n;
+
+        public Math()
+        {
+            Console.Write("Enter N for all the vectors an matrices: ");
+            n = read_Int32eger();
+        }
+
+        public class Vector
+        {
+            Double[] m_data;
+            public Vector()
+            {
+                m_data = new Double[n];
+                for (Int32 i = 0; i < n; i++)
+                    m_data[i] = 0.0;
+            }
+            public Vector sort()
+            {
+                Array.Sort(m_data);
+                return this;
+            }
+            public Vector multiply(Matrix m)
+            {
+                Vector ret = new Vector();
+                for (Int32 i = 0; i < n; i++)
+                    for (Int32 j = 0; j < n; j++)
+                        ret.m_data[i] += m_data[j] * m.get(j, i);
+                return ret;
+            }
+            public Vector sum(Vector v)
+            {
+                Vector ret = new Vector();
+                for (Int32 i = 0; i < n; i++)
+                    ret.m_data[i] = m_data[i] + v.m_data[i];
+                return ret;
+            }
+            public void set(Int32 i, Double v)
+            {
+                m_data[i] = v;
+            }
+            public Double get(Int32 i)
+            {
+                return m_data[i];
+            }
+        }
+        public class Matrix
+        {
+            Double[,] m_data;
+            public Matrix()
+            {
+                m_data = new Double[n, n];
+                for (Int32 i = 0; i < n; i++)
+                    for (Int32 j = 0; j < n; j++)
+                        m_data[i, j] = 0.0;
+            }
+            public Matrix multiply(Matrix m)
+            {
+                Matrix ret = new Matrix();
+                for (Int32 i = 0; i < n; i++)
+                    for (Int32 j = 0; j < n; j++)
+                        for (Int32 k = 0; k < n; k++)
+                            ret.m_data[i, j] += m_data[i, k] * m.m_data[k, j];
+                return ret;
+            }
+            public void swap(Int32 i, Int32 j)
+            {
+                Double t = m_data[i, j];
+                m_data[i, j] = m_data[j, i];
+                m_data[j, i] = m_data[i, j];
+            }
+            public Matrix transpose()
+            {
+                for (Int32 i = 0; i < n; i++)
+                    for (Int32 j = 0; j < i; j++)
+                        swap(i, j);
+                return this;
+            }
+            public void set(Int32 i, Int32 j, Double v)
+            {
+                m_data[i, j] = v;
+            }
+            public Double get(Int32 i, Int32 j)
+            {
+                return m_data[i, j];
+            }
+        }
+
+        public Int32 read_Int32eger()
+        {
+            return Int32.Parse(Console.ReadLine());
+        }
+        public Double read_double()
+        {
+            return Double.Parse(Console.ReadLine());
+        }
+        public Vector read_vector()
+        {
+            Vector ret = new Vector();
+            for (Int32 i = 0; i < n; i++)
+                ret.set(i, read_double());
+            return ret;
+        }
+        public Matrix read_matrix()
+        {
+            Matrix ret = new Matrix();
+            for (Int32 i = 0; i < n; i++)
+                for (Int32 j = 0; j < n; j++)
+                    ret.set(i, j, read_double());
+            return ret;
+        }
+
+        public Vector fill_vector(Double value)
+        {
+            Vector ret = new Vector();
+            for (Int32 i = 0; i < n; i++)
+                ret.set(i, value);
+            return ret;
+        }
+        public Matrix fill_matrix(Double value)
+        {
+            Matrix ret = new Matrix();
+            for (Int32 i = 0; i < n; i++)
+                for (Int32 j = 0; j < n; j++)
+                    ret.set(i, j, value);
+            return ret;
+        }
+        public void print(Vector v)
+        {
+            for (Int32 i = 0; i < n && n <= 10; i++)
+                Console.Write(v.get(i).ToString() + " ");
+            Console.WriteLine();
+        }
+        public void print(Matrix m)
+        {
+            for (Int32 i = 0; i < n && n <= 10; i++)
+            {
+                for (Int32 j = 0; j < n && n <= 10; j++)
+                    Console.Write(m.get(i, j).ToString() + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public Vector function_1(Vector a, Vector b, Matrix ma, Matrix me)
+        {
+            return a.sort().multiply(ma.multiply(me)).sum(b.sort());
+        }
+        public Matrix function_2(Matrix mk, Matrix mh, Matrix mf)
+        {
+            return mk.transpose().multiply(mh.multiply(mf));
+        }
+        public Vector function_3(Vector r, Vector s, Matrix mt, Matrix mp)
+        {
+            return r.sum(s).sort().multiply(mt.multiply(mp));
+        }
+    }
+};
