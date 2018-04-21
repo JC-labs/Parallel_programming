31ab38f3ee7b8dc39c1ca183694ba54e35fc9682
jc-labs <spam_box_jc@zoho.eu>
2018-04-21 16:22:22 +0300

    LocalMemorySystem was added as a copy of the SharedMemorySystem.

---
+++ Course_Project/LocalMemorySystem/Source.cpp
@@ -0,0 +1,28 @@
+#include "Solution.hpp"
+#include "Testing.hpp"
+
+int main() {
+	std::string s;
+	std::ifstream fn;
+	fn.open("data\\n.txt");
+	std::ofstream ft;
+	ft.open("data\\testing.txt");
+	fn >> s;
+	if (s == "testing") {
+		int k;
+		fn >> k;
+		while (k-- > 0) {
+			int p, n;
+			fn >> p >> n;
+			testing(p, n, ft);
+			std::cout << k << '\n';
+		};
+	} else {
+		fn.clear();
+		fn.seekg(0, std::ios::beg);
+		int p, n;
+		fn >> p >> n;
+		solve<true, true, false, false, true>(p, n);
+	}
+	return 0;
+}
