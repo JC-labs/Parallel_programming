28780376c7316c9a8c20563f4f07e1137940fc8a
jc-labs <spam_box_jc@zoho.eu>
2018-04-22 12:59:58 +0300

    dot product result is now distributed properly.

--- Course_Project/LocalMemorySystem/Solution.hpp
+++ Course_Project/LocalMemorySystem/Solution.hpp
@@ -1,17 +1,24 @@
 #pragma once
 #include <mpi.h>
 #include <iostream>
+#include <functional>
 #include "..\SharedMemorySystem\Types.hpp"
 #include "..\SharedMemorySystem\FileReader.hpp"
 class Sync {
 	inline static int px;
 public:
 	static void init(int _px) { px = _px; }
-	static void send(int x, int y, int count, void *ptr) {
+	static void send(int x, int y, int count, void *ptr) { 
 		MPI_Send(ptr, count, MPI_FLOAT, y * px + x, 0, MPI_COMM_WORLD);
 	}
 	static void receive(int x, int y, int count, void *ptr) {
 		MPI_Recv(ptr, count, MPI_FLOAT, y * px + x, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	}
+	static void send(int x, int y, void *ptr) {
+		MPI_Send(ptr, 1, MPI_FLOAT, y * px + x, 0, MPI_COMM_WORLD);
+	}
+	static void receive(int x, int y, void *ptr) {
+		MPI_Recv(ptr, 1, MPI_FLOAT, y * px + x, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 };
 number dot_product(vector const& v1, int i1, vector const& v2, int i2, int size) {
@@ -21,18 +28,18 @@ number dot_product(vector const& v1, int i1, vector const& v2, int i2, int size)
 		res += v1[i1++] * v2[i2++];
 	} return res;
 }
-template<bool left_to_right> int inv(int c, int pc) { if constexpr(left_to_right) return c; else return pc - c - 1; }
+template<bool left_to_right> constexpr int inv(int c, int pc) { if constexpr(left_to_right) return c; else return pc - c - 1; }
 template<bool dir, bool print_detailed_status_info = true>
 void distribute(int x, int y, int px, int py, int size, vector &v) {
 	if (inv<dir>(x, px) == 0 && inv<dir>(y, py) == 0) {
-		if (print_detailed_status_info)
+		if constexpr (print_detailed_status_info)
 			std::cout << '\t' << x << ' ' << y << " starts sending the vector : " << v.size() << "\n";
 		if (inv<dir>(y, py) != py - 1) Sync::send(x, y + (dir ? 1 : -1), (py - 1) * px * size, &v[dir ? px * size : 0]);
 		if (inv<dir>(x, px) != px - 1) Sync::send(x + (dir ? 1 : -1), y, (px - 1) * size, &v[dir ? size : (py - 1) * px * size]);
 	} else if (inv<dir>(x, px) == px - 1 && inv<dir>(y, py) == py - 1) {
 		resize(v, (px - inv<dir>(x, px)) * size);
-		Sync::receive(x - (dir ? 1 : -1), y, (px - inv<dir>(x, px)) * size, &v[0]);
-		if (print_detailed_status_info)
+		if (inv<dir>(x, px) != 0) Sync::receive(x - (dir ? 1 : -1), y, (px - inv<dir>(x, px)) * size, &v[0]);
+		if constexpr (print_detailed_status_info)
 			std::cout << '\t' << x << ' ' << y << " has received part of the vector : " << v.size() << "\n";
 	} else {
 		if (inv<dir>(x, px) == 0) {
@@ -44,8 +51,42 @@ void distribute(int x, int y, int px, int py, int size, vector &v) {
 			Sync::receive(x - (dir ? 1 : -1), y, (px - inv<dir>(x, px)) * size, &v[0]);
 		}
 		if (inv<dir>(x, px) != px - 1) Sync::send(x + (dir ? 1 : -1), y, (px - inv<dir>(x, px) - 1) * size, &v[dir ? size : (x == px - 1 ? y * px * size : 0)]);
-		if (print_detailed_status_info)
+		if constexpr (print_detailed_status_info)
 			std::cout << '\t' << x << ' ' << y << " has received part of the vector : " << v.size() << "\n";
+	}
+}
+template<bool dir, bool print_detailed_status_info = true>
+void distribute(int x, int y, int px, int py, int size, number &value, 
+				std::function<number(number, number)> lambda 
+				= [](number a, number b) -> number { return a; }) {
+	if (inv<dir>(x, px) == 0 && inv<dir>(y, py) == 0) {
+		if constexpr (print_detailed_status_info)
+			std::cout << '\t' << x << ' ' << y << " starts sending its value : " << value << "\n";
+		if (inv<dir>(x, px) != px - 1) Sync::send(x + (dir ? 1 : -1), y, &value);
+	} else if (inv<dir>(x, px) == px - 1 && inv<dir>(y, py) == py - 1) {
+		number temp;
+		if (inv<dir>(x, px) != 0) Sync::receive(x - (dir ? 1 : -1), y, &temp);
+		value = lambda(value, temp);
+		if (inv<dir>(y, py) != 0) Sync::receive(x, y - (dir ? 1 : -1), &temp);
+		value = lambda(value, temp);
+		if constexpr (print_detailed_status_info)
+			std::cout << '\t' << x << ' ' << y << " has calculated its value : " << value << "\n";
+	} else {
+		number temp;
+		if (inv<dir>(x, px) == px - 1 && inv<dir>(y, py) != 0) {
+			Sync::receive(x, y - (dir ? 1 : -1), &temp);
+			value = lambda(value, temp);
+		}
+		if (inv<dir>(x, px) != 0) {
+			Sync::receive(x - (dir ? 1 : -1), y, &temp);
+			value = lambda(value, temp);
+		}
+		if (inv<dir>(x, px) == px - 1 && inv<dir>(y, py) != py - 1)
+			Sync::send(x, y + (dir ? 1 : -1), &value);
+		else if (inv<dir>(x, px) != px - 1)
+			Sync::send(x + (dir ? 1 : -1), y, &value);
+		if constexpr (print_detailed_status_info)
+			std::cout << '\t' << x << ' ' << y << " has calculated its value : " << value << "\n";
 	}
 }
 template <bool output = true, bool status_print = true, bool print_detailed_status_info = true, bool print_error_info = true>
@@ -60,7 +101,8 @@ void solve(int px, int py, int n) {
 		std::cout << ("MPI 3.0 isn't supported.");
 	{
 		int id, p; MPI_Comm_rank(MPI_COMM_WORLD, &id); MPI_Comm_size(MPI_COMM_WORLD, &p);
-		if (print_error_info && p != px * py) std::cout << "It's impossible to create specified number(" << px * py << ") of threads.";
+		if (print_error_info && p != px * py) 
+			std::cout << "It's impossible to create specified number(" << px * py << ") of threads.";
 
 		int y = id / px;
 		int x = id - y * px;
@@ -81,7 +123,14 @@ void solve(int px, int py, int n) {
 		distribute<false, print_detailed_status_info>(x, y, px, py, size, b);
 
 		number d = dot_product(b, id == 0 ? 0 : b.size() - size, c, 0, size);
-		std::cout << '\t' << x << ' ' << y << " has calculated b * c. Result is equal to " << d << '\n';
+		if constexpr (status_print) 
+			std::cout << '\t' << x << ' ' << y << " has calculated b * c. Result is equal to " << d << '\n';
+
+		distribute<true, print_detailed_status_info>(x, y, px, py, size, d, [](number a, number b) -> number { 
+			return a + b; 
+		});
+		if constexpr (status_print) 
+			std::cout << '\t' << x << ' ' << y << " has sent d with vaule: " << d << '\n';
 
 		if (status_print) std::cout << "Thread #" << id << " has finished.\n";
 	} MPI_Finalize();
