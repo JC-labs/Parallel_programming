17a9ec12a6a70a83996c33aae2cdbe98ff5d7a15
jc-labs <spam_box_jc@zoho.eu>
2018-04-22 05:09:27 +0300

    Dot product calculations are now complete. Old commented code was removed.

--- Course_Project/LocalMemorySystem/Solution.hpp
+++ Course_Project/LocalMemorySystem/Solution.hpp
@@ -16,6 +16,13 @@ public:
 		MPI_Recv(ptr, count, MPI_FLOAT, y * px + x, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 };
+number dot_product(vector const& v1, int i1, vector const& v2, int i2, int size) {
+	number res = 0;
+	size += i1;
+	while (i1 < size) {
+		res += v1[i1++] * v2[i2++];
+	} return res;
+}
 template <bool output = true, bool status_print = true, bool print_detailed_status_info = true, bool print_error_info = true>
 void solve(int px, int py, int n) {
 	int size = n / (px * py);
@@ -30,7 +37,7 @@ void solve(int px, int py, int n) {
 		int id, p; 
 		MPI_Comm_rank(MPI_COMM_WORLD, &id);
 		MPI_Comm_size(MPI_COMM_WORLD, &p);
-		if (print_error_info && p != px * py) std::cout << ("It's impossible to create specified number of threads.");
+		if (print_error_info && p != px * py) std::cout << "It's impossible to create specified number(" << px * py << ") of threads.";
 
 		int y = id / px;
 		int x = id - y * px;
@@ -39,49 +46,75 @@ void solve(int px, int py, int n) {
 			str << "Thread #" << id << " has started.\n";
 			std::cout << str.str();
 		}
-
-		int start = size * id;
-		int finish = start + size;
-
+		
+		vector c, b, z;
+		matrix mo, ms, mr;
 		if (id == 0) {
-			vector c; resize(c, n);
-			matrix mo; resize(mo, n);
-			matrix ms; resize(ms, n);
+			resize(c, n); resize(mo, n); resize(ms, n);
 			read_file("data\\input_0.txt", c, mo, ms);
-			write_file("data\\output.txt", mo);
 
 			if (print_detailed_status_info) 
 				std::cout << '\t' << x << ' ' << y << " starts sending c : " << c.size() << "\n";
 			if (y != py - 1) Sync::send(x, y + 1, n - (px * size), &c[px * size]);
 			if (x != px - 1) Sync::send(x + 1, y, (px - 1) * size, &c[size]);
+			
+			if (id != p - 1) {
+				resize(b, (x + 1) * size);
+				Sync::receive(x + 1, y, (x + 1) * size, &b[0]);
+				if (print_detailed_status_info)
+					std::cout << '\t' << x << ' ' << y << " has received part of b : " << b.size() << "\n";
+			}
+
+			auto d = dot_product(b, 0, c, 0, size);
+			std::cout << '\t' << x << ' ' << y << " has calculated b * c. Result is equal to " << d << '\n';			
 		}
 		if (id == p - 1) {
-			vector b; resize(b, n);
-			vector z; resize(z, n);
-			matrix mr; resize(mr, n);
+			resize(b, n); resize(z, n); resize(mr, n);
 			read_file("data\\input_1.txt", b, z, mr);
 
-			vector c; resize(c, (px - x) * size);
-			Sync::receive(x - 1, y, (px - x) * size, &c[0]);
-			if (print_detailed_status_info) 
-				std::cout << '\t' << x << ' ' << y << " has received part of c : " << c.size() << "\n";
-		} 
-		if (id != 0 && id != p - 1) {
-			vector c;
-			if (x == 0) {
-				resize(c, (py - y) * px * size);
-				Sync::receive(x, y - 1, (py - y) * px * size, &c[0]);
-				if (y != py - 1) Sync::send(x, y + 1, (px - x - 1) * size, &c[px * size]);
-				if (x != px - 1) Sync::send(x + 1, y, (px - 1) * size, &c[size]);
-				if (print_detailed_status_info)
-					std::cout << '\t' << x << ' ' << y << " has received part of c : " << c.size() << "\n";
-			} else {
+			if (id != 0) {
 				resize(c, (px - x) * size);
 				Sync::receive(x - 1, y, (px - x) * size, &c[0]);
-				if (x != px - 1) Sync::send(x + 1, y, (px - x - 1) * size, &c[size]);
 				if (print_detailed_status_info)
 					std::cout << '\t' << x << ' ' << y << " has received part of c : " << c.size() << "\n";
 			}
+			
+			if (print_detailed_status_info)
+				std::cout << '\t' << x << ' ' << y << " starts sending b : " << b.size() << "\n";
+			if (y != 0) Sync::send(x, y - 1, n - (px * size), &b[0]);
+			if (x != 0) Sync::send(x - 1, y, (px - 1) * size, &b[n - (px * size)]);
+
+			auto d = dot_product(b, n - size - 1, c, 0, size);
+			std::cout << '\t' << x << ' ' << y << " has calculated b * c. Result is equal to " << d << '\n';			
+		} 
+		if (id != 0 && id != p - 1) {
+			if (x == 0) {
+				resize(c, (py - y) * px * size);
+				Sync::receive(x, y - 1, (py - y) * px * size, &c[0]);
+				if (y != py - 1) Sync::send(x, y + 1, (py - y - 1) * px * size, &c[px * size]);
+			} else {
+				resize(c, (px - x) * size);
+				Sync::receive(x - 1, y, (px - x) * size, &c[0]);
+			}
+			if (x != px - 1) Sync::send(x + 1, y, (px - x - 1) * size, &c[size]);
+			if (print_detailed_status_info)
+				std::cout << '\t' << x << ' ' << y << " has received part of c : " << c.size() << "\n";
+			
+			if (x == px - 1) {
+				resize(b, (y + 1) * px * size);
+				Sync::receive(x, y + 1, (y + 1) * px * size, &b[0]);
+				if (y != 0) Sync::send(x, y - 1, y * px * size, &b[0]);
+				if (x != 0) Sync::send(x - 1, y, x * size, &b[y * px * size]);
+			} else {
+				resize(b, (x + 1) * size);
+				Sync::receive(x + 1, y, (x + 1) * size, &b[0]);
+				if (x != 0) Sync::send(x - 1, y, x * size, &b[0]);
+			}
+			if (print_detailed_status_info)
+				std::cout << '\t' << x << ' ' << y << " has received part of b : " << b.size() << "\n";
+
+			auto d = dot_product(b, b.size() - size, c, 0, size);
+			std::cout << '\t' << x << ' ' << y << " has calculated b * c. Result is equal to " << d << '\n';
 		}
 
 		if (status_print) {
@@ -90,124 +123,4 @@ void solve(int px, int py, int n) {
 			std::cout << str.str();
 		}
 	} MPI_Finalize();
-
-		/*
-		#pragma omp barrier
-
-		if (no_temp) {
-			if (for_blocks) {
-				#pragma omp for reduction(+:d) nowait
-				for (int i = 0; i < n; i++)
-					d += m.b[i] * m.c[i];
-				#pragma omp for nowait
-				for (int i = 0; i < n; i++) {
-					#pragma omp critical(cs_max)
-					{
-						if (m.z[i] > e)
-							e = m.z[i];
-					}
-				}
-			} else {
-				for (int i = start; i < finish; i++) {
-					#pragma omp atomic
-						d += m.b[i] * m.c[i];
-				}
-				for (int i = start; i < finish; i++) {
-					#pragma omp critical(cs_max)
-					{
-						if (m.z[i] > e)
-							e = m.z[i];
-					}
-				}
-			}
-		} else {
-			number d_i = 0, e_i = std::numeric_limits<number>::min();
-			if (for_blocks) {
-				#pragma omp for
-				for (int i = 0; i < n; i++)
-					d_i += m.b[i] * m.c[i];
-				#pragma omp atomic
-				d += d_i;
-				#pragma omp for
-				for (int i = 0; i < n; i++)
-					if (m.z[i] > e_i)
-						e_i = m.z[i];
-				#pragma omp critical(cs_max)
-				{
-					if (e_i > e) e = e_i;
-				}
-			} else {
-				for (int i = start; i < finish; i++)
-					d_i += m.b[i] * m.c[i];
-				#pragma omp atomic
-					d += d_i;
-				for (int i = 0; i < n; i++)
-					if (m.z[i] > e_i)
-						e_i = m.z[i];
-				#pragma omp critical(cs_max)
-				{
-					if (e_i > e) e = e_i;
-				}
-			}
-		}
-
-		#pragma omp barrier
-
-		if (copy) {
-			matrix ms_i; 
-			number d_i, e_i;
-			resize(ms_i, n);
-			#pragma omp critical(cs_copy)
-			{
-				d_i = d;
-				e_i = e;
-			}
-			omp_set_lock(&ms_copy);
-			std::copy(m.ms.begin(), m.ms.end(), ms_i.begin());
-			omp_unset_lock(&ms_copy);
-
-			if (for_blocks) {
-				#pragma omp for nowait
-				for (int i = 0; i < n; i++)
-					for (int j = 0; j < n; j++) {
-						number t = 0;
-						for (int k = 0; k < n; k++)
-							t += m.mr[i][k] * ms_i[k][j];
-						m.ma[i][j] = m.mo[i][j] * d_i + t * e_i;
-					}
-			} else {
-				for (int i = start; i < finish; i++)
-					for (int j = 0; j < n; j++) {
-						number t = 0;
-						for (int k = 0; k < n; k++)
-							t += m.mr[i][k] * ms_i[k][j];
-						m.ma[i][j] = m.mo[i][j] * d_i + t * e_i;
-					}
-			}
-		} else {
-			if (for_blocks) {
-				#pragma omp for nowait
-				for (int i = 0; i < n; i++)
-					for (int j = 0; j < n; j++) {
-						number t = 0;
-						for (int k = 0; k < n; k++)
-							t += m.mr[i][j] * m.ms[i][j];
-						m.ma[i][j] = m.mo[i][j] * d + t * e;
-					}
-			} else {
-				for (int i = start; i < finish; i++)
-					for (int j = 0; j < n; j++) {
-						number t = 0;
-						for (int k = 0; k < n; k++)
-							t += m.mr[i][j] * m.ms[i][j];
-						m.ma[i][j] = m.mo[i][j] * d + t * e;
-					}
-			}
-		}
-
-		#pragma omp barrier
-		if (output && t_id == p - 1)
-			write_file("data\\output.txt", m.ma);
-	}
-	if constexpr (copy) omp_destroy_lock(&ms_copy);*/
 }
