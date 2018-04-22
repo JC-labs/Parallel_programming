58d573c37a4ad6670d47ea44fafae2dffcc57d9e
jc-labs <spam_box_jc@zoho.eu>
2018-04-22 14:16:42 +0300

    dot_product and maximum functions were added for vectors.

--- Course_Project/SharedMemorySystem/Types.cpp
+++ Course_Project/SharedMemorySystem/Types.cpp
@@ -18,11 +18,12 @@ void get(matrix &m, std::istream &s) {
 			s >> it;
 }
 void resize(vector &v, size_t n) { v.resize(n); }
-void resize(matrix &m, size_t n) {
-	m.resize(n);
+void resize(matrix &m, size_t nx, size_t ny) {
+	m.resize(nx);
 	for (auto &it : m)
-		it.resize(n);
+		it.resize(ny);
 }
+void resize(matrix &m, size_t n) { return resize(m, n, n); }
 void write_file(matrix const& m, std::ostream &s) {
 	s << "Output of a matrix (" << m.size() << "x" << m.size() << "):\n";
 	for (auto v : m) {
@@ -36,4 +37,18 @@ void write_file(vector const& v, std::ostream &s) {
 		for (auto it : v)
 			s << it << ' ';
 		s << '\n';
+}
+number dot_product(vector const& v1, int i1, vector const& v2, int i2, int size) {
+	number res = 0;
+	size += i1;
+	while (i1 < size) {
+		res += v1[i1++] * v2[i2++];
+	} return res;
+}
+number maximum(vector const& v, int i, int size) {
+	number res = std::numeric_limits<number>::min();
+	size += i;
+	while (i < size)
+		if (v[i++] > res) res = v[i - 1];
+	return res;
 }
--- Course_Project/SharedMemorySystem/Types.hpp
+++ Course_Project/SharedMemorySystem/Types.hpp
@@ -11,5 +11,8 @@ void get(vector &v, std::istream &s);
 void get(matrix &m, std::istream &s);
 void resize(vector &v, size_t n);
 void resize(matrix &m, size_t n);
+void resize(matrix &m, size_t nx, size_t ny);
 void write_file(matrix const& m, std::ostream &s);
 void write_file(vector const& v, std::ostream &s);
+number dot_product(vector const& v1, int i1, vector const& v2, int i2, int size);
+number maximum(vector const& v, int i, int size);
