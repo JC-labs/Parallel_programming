dedb8d1395f270e199ccd0720ccca2891bfa1deb
jc-labs <spam_box_jc@zoho.eu>
2018-04-24 00:53:18 +0300

    Testing was configured for LMS.

--- Course_Project/LocalMemorySystem/Testing.hpp
+++ Course_Project/LocalMemorySystem/Testing.hpp
@@ -12,20 +12,5 @@ size_t test(function f, args...a) {
 int test_id = 0;
 void testing(int px, int py, int n, std::ostream &s = std::cout) {
 	s << "Testing series #" << test_id++ << " (p = " << px << "x" << py << ", n = " << n << "):\n"
-		<< "\tTest #0 (w/o copies, w/o temporary variables, w/o openMP-style for loops)\n"
-		<< "\t\ttook " << test(solve<>, px, py, n) << " microseconds.\n"
-		;//<< "\tTest #1 (w/o copies, w/o temporary variables, w/ openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ false, false, true>, p, n) << " microseconds.\n"
-		//<< "\tTest #2 (w/o copies, w/ temporary variables, w/o openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ false, true, false>, p, n) << " microseconds.\n"
-		//<< "\tTest #3 (w/o copies, w/ temporary variables, w/ openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ false, true, true>, p, n) << " microseconds.\n"
-		//<< "\tTest #4 (w/ copies, w/o temporary variables, w/o openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ true, false, false>, p, n) << " microseconds.\n"
-		//<< "\tTest #5 (w/ copies, w/o temporary variables, w/ openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ true, false, true>, p, n) << " microseconds.\n"
-		//<< "\tTest #6 (w/ copies, w/ temporary variables, w/o openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ true, true, false>, p, n) << " microseconds.\n"
-		//<< "\tTest #7 (w/ copies, w/ temporary variables, w/ openMP-style for loops)\n"
-		//<< "\t\ttook " << test(solve<false, false, /**/ true, true, true>, p, n) << " microseconds.\n";
+		<< "\tTest took " << test(solve<>, px, py, n) << " microseconds.\n";
 }
