c27806ab45cb82d77ae044ab10e3341781258864
jc-labs <spam_box_jc@zoho.eu>
2018-05-11 02:47:08 +0300

    Temporary progress.

--- 2/Lab8/Lab8.cpp
+++ 2/Lab8/Lab8.cpp
@@ -1,56 +1,91 @@
 /*
     Parallel and Allotted Calculations.
-    Lab #4: OpenMP: mutexes, bariers, critical sections and parallel loops.
+    Lab #8: MPI: distributed graph.
 
     The code was written by
         Krisak Ivan,
         student of IO-53, KPI.
-    April 18, 2018.
+    May 4th, 2018.
 
     Function:
-	A = max(Z) * E + (T * MO) * (MK * MR);
+	MA = max(Z) * MO + e * (MT * MR);
 */
 #include "../../Course_Project/SharedMemorySystem/FileReader.hpp"
 #include "../../Course_Project/SharedMemorySystem/Types.cpp"
-#include <omp.h>
+#include <mpi.h>
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <algorithm>
-struct Memory {
-	int n, p, size;
-
-	vector a, z, e, t;
-	matrix mo, mk, mr;
-
-	number b;
-	vector c;
-
-	omp_lock_t mutex;
-
-	void clear(int n) {
-		b = std::numeric_limits<number>::min();
-		::resize(a, n);
-		::resize(z, n);
-		::resize(e, n);
-		::resize(t, n);
-		::resize(mo, n);
-		::resize(mk, n);
-		::resize(mr, n);
-
-		::resize(c, n);
-	}
-	Memory(int _n, int _p) {
-		n = _n; p = _p; size = _n / _p;
-		clear(_n);
-		omp_init_lock(&mutex);
-	}
-	~Memory() {
-		omp_destroy_lock(&mutex);
-	}
-};
-
 int main() {
+	int id, n, p, size;
+	vector z;
+	matrix mo, mt, mr;
+	number e;
+	number b;
+	matrix ma;
+
+	int is_provided, is_successful = MPI_Init_thread(nullptr, nullptr, MPI_THREAD_MULTIPLE, &is_provided);
+	if (is_successful != MPI_SUCCESS || is_provided != MPI_THREAD_MULTIPLE)
+		std::cout << ("MPI 3.0 isn't supported.");
+	MPI_Comm_rank(MPI_COMM_WORLD, &id); MPI_Comm_size(MPI_COMM_WORLD, &p);
+	if (id == 0) {
+		std::ifstream fn;
+		fn.open("data/n.txt");
+		fn >> n;
+		z.resize(n); mo.resize(n); mt.resize(n); mr.resize(n);
+		read_file("data/input.txt", z, mo, mt, mr, e);
+	}
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	size = n / p;
+
+	MPI_Group group;
+	MPI_Comm graph_comm;
+	int const topology_size = 11;
+	int const indices[topology_size] = {4, 6, 8, 10, 13, 14, 15, 17, 18, 19, 20},
+		edges[topology_size * 2] = { 1, 2, 3, 4,
+					0, 5,
+					0, 6,
+					0, 7,
+					0, 8, 9,
+					1,
+					2,
+					3, 10,
+					4,
+					4,
+					7
+	};
+	if (p != topology_size) {
+		std::cout << "Unsupported number of processors... Aborting.\n";
+		std::cin.get();
+		return 0;
+	}
+	MPI_Graph_create(MPI_COMM_WORLD, topology_size, indices, edges, false, &graph_comm);
+	MPI_Comm_group(graph_comm, &group);
+
+	//MPI_Intercomm_create(graph_comm, 0, )
+
+	std::cout << "Process #" << id << " has been initialized.\n";
+	if (n >= p) {
+		number *z_l = new number[size];
+		//z.resize(size);
+		//MPI_Scatter((void*) z.data(), n, MPI_FLOAT, MPI_IN_PLACE, size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		if (id == 0)
+			MPI_Scatter((void*) z.data(), n, MPI_FLOAT, z_l, size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		else {
+			//z.resize(size);
+			MPI_Scatter(MPI_IN_PLACE, n, MPI_FLOAT, z_l, size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		}
+		//MPI_Bcast(z.data(), n, MPI_FLOAT, 0, graph_comm);
+		std::cout << "My local Z is ";
+		for (int i = 0; i < size; i++)/*/(auto it : z)*/
+			std::cout << z_l[i]/*/it*/ << '\t';
+		std::cout << std::endl;
+	}
+
+	std::cout << "Process #" << id << " has been finished.\n";
+	MPI_Comm_free(&graph_comm);
+	MPI_Finalize();
+	/*
 	int n;
 	std::ifstream fn;
 	fn.open("data/n.txt");
@@ -106,4 +141,5 @@ int main() {
 		std::cout << "Thread #" << id + 1 << " has been resolved.\n";
 	}
 	return 0;
+	*/
 }
